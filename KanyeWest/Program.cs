44cbdcee4de47923c4d0041aa5d959526749e5f8
Nolan Martin <nmartinsdtc@gmail.com>
2023-04-10 09:56:12 -0600

    Yeezy vs. Ron Swanson

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Newtonsoft.Json.Linq;
+
+var client1 = new HttpClient();
+var client2 = new HttpClient();
+
+var kanyeURL = "https://api.kanye.rest/";
+var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+for (int i = 0; i <5; i++)
+{
+    var kanyeResponse = client1.GetStringAsync(kanyeURL).Result;
+    var ronResponse = client2.GetStringAsync(ronURL).Result;
+
+    var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+    var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+    Console.WriteLine($"Yeezy: {kanyeQuote}");
+    Console.WriteLine($"Ron: {ronQuote}");
+    Console.WriteLine();
+}
